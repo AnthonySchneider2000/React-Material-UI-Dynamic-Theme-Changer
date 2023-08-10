eb87aebe18eb3400b74f000cf5fc621d7bcc8e93
Anthony Schneider <tonyschneider3@gmail.com>
2023-08-10 00:37:47 -0500

    indentation

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -1,75 +1,86 @@
-import * as React from 'react';
+import * as React from "react";
 import { Link } from "react-router-dom"; // Import the Link component from react-router-dom
 import HomeIcon from "@mui/icons-material/Home";
 import InboxIcon from "@mui/icons-material/Inbox";
 import SearchIcon from "@mui/icons-material/Search";
 import { ColorLens } from "@mui/icons-material";
 import { Brightness2, Brightness7 } from "@mui/icons-material";
-import { ListItem, TextField } from '@mui/material';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import { ListItem, TextField } from "@mui/material";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PeopleIcon from "@mui/icons-material/People";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import LayersIcon from "@mui/icons-material/Layers";
+import AssignmentIcon from "@mui/icons-material/Assignment";
 
-
-export const mainListItems = ({ handleNewMessages, onThemeChange, isDarkMode, toggleDarkMode, colorPickerColor, handleColorChange }) => (
+export const mainListItems = ({
+  handleNewMessages,
+  onThemeChange,
+  isDarkMode,
+  toggleDarkMode,
+  colorPickerColor,
+  handleColorChange,
+}) => (
   <React.Fragment>
     <ListItem button component={Link} to="/">
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
-        <ListItem button component={Link} to="/dashboard">
-          <ListItemIcon>
-            <DashboardIcon />
-          </ListItemIcon>
-          <ListItemText primary="Dashboard" />
-        </ListItem>
-        <ListItem button component={Link} to="/profile">
-          <ListItemIcon>
-            <AccountBoxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Profile" />
-        </ListItem>
-        <ListItem button onClick={handleNewMessages}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="New Messages" />
-        </ListItem>
-        <ListItem button sx={{maxHeight:"50px"}}>
-          <ListItemIcon>
-            <SearchIcon />
-          </ListItemIcon>
-          <TextField variant="standard" label="Search" size="small"  sx={{marginBottom: "7px"}} />
-        </ListItem>
-        <ListItem button onClick={toggleDarkMode}>
-        <ListItemIcon>
-          {isDarkMode ? <Brightness7 /> : <Brightness2 />}
-        </ListItemIcon>
-        <ListItemText primary={isDarkMode ? "Light Mode" : "Dark Mode"} />
-      </ListItem>
-        <ListItem button onClick={onThemeChange}>
-          <ListItemIcon>
-            <ColorLens />
-          </ListItemIcon>
-          <ListItemText primary="Custom Theme" />
-          <input
-            style={{ backgroundColor: "transparent", border: "none" }}
-            type="color"
-            value={colorPickerColor}
-            onChange={handleColorChange}
-          />
-        </ListItem>
+      <ListItemIcon>
+        <HomeIcon />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItem>
+    <ListItem button component={Link} to="/dashboard">
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+    <ListItem button component={Link} to="/profile">
+      <ListItemIcon>
+        <AccountBoxIcon />
+      </ListItemIcon>
+      <ListItemText primary="Profile" />
+    </ListItem>
+    <ListItem button onClick={handleNewMessages}>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <ListItemText primary="New Messages" />
+    </ListItem>
+    <ListItem button sx={{ maxHeight: "50px" }}>
+      <ListItemIcon>
+        <SearchIcon />
+      </ListItemIcon>
+      <TextField
+        variant="standard"
+        label="Search"
+        size="small"
+        sx={{ marginBottom: "7px" }}
+      />
+    </ListItem>
+    <ListItem button onClick={toggleDarkMode}>
+      <ListItemIcon>
+        {isDarkMode ? <Brightness7 /> : <Brightness2 />}
+      </ListItemIcon>
+      <ListItemText primary={isDarkMode ? "Light Mode" : "Dark Mode"} />
+    </ListItem>
+    <ListItem button onClick={onThemeChange}>
+      <ListItemIcon>
+        <ColorLens />
+      </ListItemIcon>
+      <ListItemText primary="Custom Theme" />
+      <input
+        style={{ backgroundColor: "transparent", border: "none" }}
+        type="color"
+        value={colorPickerColor}
+        onChange={handleColorChange}
+      />
+    </ListItem>
   </React.Fragment>
 );
 
@@ -100,7 +111,6 @@ export const secondaryListItems = (
 );
 
 export const thirdListItems = (
-  
   <React.Fragment>
     <ListItemButton>
       <ListItemIcon>
