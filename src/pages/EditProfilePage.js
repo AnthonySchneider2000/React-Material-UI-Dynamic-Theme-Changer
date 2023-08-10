a84ac4aa43f18eadae4d604a12b6532db2496131
Anthony Schneider <tonyschneider3@gmail.com>
2023-08-09 20:11:24 -0500

    fixed styling in EditProfilePage

--- src/pages/EditProfilePage.js
+++ src/pages/EditProfilePage.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { CssBaseline, Button, TextField, Box, Toolbar } from "@mui/material";
+import {
+  CssBaseline,
+  Button,
+  TextField,
+  Box,
+  Toolbar,
+  Paper,
+  Grid,
+} from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { toast } from "react-hot-toast";
-import { Link } from "react-router-dom"; // Import the Link component from react-router-dom
 import styles from "../styles/app.module.css";
-import MuteSwitch from "../components/MuteSwitch.js";
-import StyledAvatar from "../components/StyledAvatar.js";
 import Footer from "../components/Footer";
 import { useThemeContext } from "../utils/ThemeContext";
 import { DatePicker } from "@mui/x-date-pickers";
@@ -53,56 +58,50 @@ const EditProfilePage = () => {
               because the actual toolbar is displayed from AppBarComponent*/}
           <Toolbar sx={{ height: 100 }} />
           {/* edit user info */}
-          <div className={styles.centeredContent}>
-            {/* first name entry */}
-            <TextField
-              id="outlined-basic"
-              label="First Name"
-              variant="outlined"
-              className={styles.inputField}
-            />
-            {/* last name entry */}
-            <TextField
-              id="outlined-basic"
-              label="Last Name"
-              variant="outlined"
-              className={styles.inputField}
-            />
-            {/* date of birth entry */}
-            <DatePicker label="Date of Birth" className={styles.inputField} />
-            {/* phone number entry */}
-            <MuiPhoneNumber
-              variant="outlined"
-              label="Phone Number"
-              defaultCountry={"us"}
-              className={styles.inputField}
-            />
-          </div>
-          <div className={styles.saveButtonContainer}>
-            {/* save button */}
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleSaveSettings}
-              className={styles.saveButton}
-            >
-              Save
-            </Button>
-          </div>
-
-          {/* mute switch */}
-          <div className={styles.muteSwitch}>
-            <MuteSwitch />
-          </div>
-          {/* avatar */}
-          <Link
-            to="/profile"
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            <div className={styles.avatar}>
-              <StyledAvatar>TS</StyledAvatar>
-            </div>
-          </Link>
+          <Grid container spacing={3} direction="column" justifyContent="center" alignItems="center">
+            <Grid item xs={12} md={8} lg={9}>
+              {/* first name entry */}
+              <TextField
+                id="outlined-basic"
+                label="First Name"
+                variant="outlined"
+                className={styles.inputField}
+              />
+            </Grid>
+            <Grid item xs={12} md={8} lg={9}>
+              {/* last name entry */}
+              <TextField
+                id="outlined-basic"
+                label="Last Name"
+                variant="outlined"
+                className={styles.inputField}
+              />
+            </Grid>
+            <Grid item xs={12} md={8} lg={9}>
+              {/* date of birth entry */}
+              <DatePicker label="Date of Birth" className={styles.inputField} />
+            </Grid>
+            <Grid item xs={12} md={8} lg={9}>
+              {/* phone number entry */}
+              <MuiPhoneNumber
+                variant="outlined"
+                label="Phone Number"
+                defaultCountry={"us"}
+                className={styles.inputField}
+              />
+            </Grid>
+            <Grid item xs={12} md={8} lg={9}>
+              {/* save button */}
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSaveSettings}
+                className={styles.saveButton}
+              >
+                Save
+              </Button>
+            </Grid>
+          </Grid>
         </Box>
         {/* footer */}
         <Footer currentTheme={currentTheme} />
